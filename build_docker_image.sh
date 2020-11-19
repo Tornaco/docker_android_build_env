07dfd218e13407404c8056350ae01cdd93753fd3
tornaco <tornaco@163.com>
2020-11-19 14:08:00 +0800

    Update scripts

---
+++ build_docker_image.sh
@@ -0,0 +1,1 @@
+docker build -t tornaco/android_builder:latest .
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --volume /Volumes/T5/docker/volumes/android_builder/home:/home -it tornaco/android_builder
+docker run --volume /Users/twadmin/Documents/android-build-home:/home -it tornaco/android_builder
